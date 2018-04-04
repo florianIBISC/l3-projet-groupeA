c6ad5d3859d933cc7883a538e7ef491007cd488f
vignesh_patcheappane <vignesh91350@outlook.fr>
2018-04-04 15:19:56 +0200

    izi

--- Project_School_1.0/src/application/Main.java
+++ Project_School_1.0/src/application/Main.java
@@ -22,6 +22,6 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
-		System.out.println("Oh yeah");
+		System.out.println("Oh yeah je suis Vignesh !!");
 	}
 }
