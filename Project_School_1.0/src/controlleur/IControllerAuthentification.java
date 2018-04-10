686a09333e612e5a3187f07b9294f4e9346dbaff
flo-pro <jeanjeandu91@gmail.com>
2018-04-10 22:19:58 +0200

    Revert "Revert "quelques reorganisations""
    This reverts commit 7b8f1b7a4e78b98db3023fdf23802a2add2c8676.

--- Project_School_1.0/src/controlleur/IControllerAuthentification.java
+++ Project_School_1.0/src/controlleur/IControllerAuthentification.java
@@ -4,7 +4,6 @@ public interface IControllerAuthentification {
 	public boolean id_correct(String id);
 	public boolean mdp_correct(String mdp);
 	public boolean compte_bloque();
-	
 	//Décremente le nombre de tentative possible de déverouillage du compte
 	public void decrementerNbrTentative();
 	
